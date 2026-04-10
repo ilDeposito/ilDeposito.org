7cd5d10d56fd9d8461319e52b8ee36287c1f6af3
Sergio Durzu <sergio.durzu@gmail.com>
2026-04-10 13:53:28 +0200

    chore: remove compiled theme assets from version control

--- web/themes/custom/ildeposito/components/header/header/header.js
+++
@@ -1,8 +0,0 @@
-/******/ (function() { // webpackBootstrap
-/*!*********************************************!*\
-  !*** ./components/header/header/_header.js ***!
-  \*********************************************/
-// Rimosso: Bootstrap Modal gestito via markup, nessun JS custom necessario.
-/******/ })()
-;
-//# sourceMappingURL=header.js.map
